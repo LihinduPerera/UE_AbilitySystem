bf00d475bd8841f5033a10bd23743724294e1d72
LihinduPerera <lihindu.indudunu.perera@gmail.com>
2025-11-27 16:32:26 +0530

    Fixed Visual Studio IntelliSense vs Unreal Build Tool mismatch

--- Source/UE_AbilitySystem/UE_AbilitySystem.Build.cs
+++ Source/UE_AbilitySystem/UE_AbilitySystem.Build.cs
@@ -1,5 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+using System.IO;
 using UnrealBuildTool;
 
 public class UE_AbilitySystem : ModuleRules
@@ -12,12 +13,16 @@ public class UE_AbilitySystem : ModuleRules
 
 		PrivateDependencyModuleNames.AddRange(new string[] { "GameplayAbilities", "GameplayTasks", "GameplayTags" });
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+        PublicIncludePaths.Add(ModuleDirectory); // Add the module root
+        PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "AttributeSets")); // Subfolder
+        PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "Characters")); // Subfolder
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+        // Uncomment if you are using Slate UI
+        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+        // Uncomment if you are using online features
+        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+    }
 }
